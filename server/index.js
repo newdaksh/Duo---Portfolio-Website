fd86a5ff52ffcc253b4286b19cf5204f53859284
newdaksh <dakshj4@gmail.com>
2025-08-08 23:14:11 +0530

    index.js location change

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import emailRouter from "./routes/email.js";
+import emailRouter from "./src/routes/email.js";
 
 dotenv.config();
 
